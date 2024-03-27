913fe3304369c0936e2504f4aeb7bf079920d451
Aysenem ZOBALAK <yokaii.noodle@gmail.com>
2024-03-27 06:27:03 +0300

    EnemyTriggerArea added.
    EnemyTriggerArea added.

--- PolyDungeons/Assets/Scripts/NPC/NPC.cs
+++ PolyDungeons/Assets/Scripts/NPC/NPC.cs
@@ -6,11 +6,9 @@ public class NPC : MonoBehaviour
 {
     public Transform hedefNesne; // Karakterin Transform bileşeni
     public float hareketHizi = 3f; // NPC'nin hareket hızı
+    private bool takipDurum = false; // Takip durumu
 
     private Animator _anim;
-
-    // 1,5 f yürümehızı  
-
 
     private void Start()
     {
@@ -19,17 +17,20 @@ public class NPC : MonoBehaviour
 
     void Update()
     {
-        // NPC'nin ve karakterin konumunu al
-        Vector3 npcKonumu = transform.position;
-        Vector3 hedefKonum = hedefNesne.position;
+        if (takipDurum && hedefNesne != null)
+        {
+            // NPC'nin ve hedefin konumunu al
+            Vector3 npcKonumu = transform.position;
+            Vector3 hedefKonum = hedefNesne.position;
 
-        // NPC'nin karakterin konumuna doğru ilerlemesini sağla
-        transform.position = Vector3.MoveTowards(npcKonumu, hedefKonum, hareketHizi * Time.deltaTime);
+            // NPC'nin karakterin konumuna doğru ilerlemesini sağla
+            transform.position = Vector3.MoveTowards(npcKonumu, hedefKonum, hareketHizi * Time.deltaTime);
 
-        // NPC'nin hedef nesneye doğru bakmasını sağla
-        transform.LookAt(hedefKonum);
+            // NPC'nin hedef nesneye doğru bakmasını sağla
+            transform.LookAt(hedefKonum);
 
-        Anim();
+            Anim();
+        }
     }
 
     private void Anim()
@@ -40,4 +41,39 @@ public class NPC : MonoBehaviour
             Debug.Log("Koşuyorrrrrrrrr");
         }
     }
+
+    // Tetikleyici alana girildiğinde çalışacak metod
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            takipDurum = true;
+            hedefNesne = other.transform; // Oyuncunun pozisyonunu hedef olarak ayarla
+        }
+    }
+
+    // Tetikleyici alandan çıkıldığında çalışacak metod
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            takipDurum = false;
+            hedefNesne = null; // Hedefi boşalt
+        }
+    }
+    
+    #region EnemyTigger
+    public void BaşlaTakip(Transform hedef)
+    {
+        takipDurum = true;
+        hedefNesne = hedef;
+    }
+
+    public void DurTakip()
+    {
+        takipDurum = false;
+        hedefNesne = null;
+    }
+    #endregion
+
 }
